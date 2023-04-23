fc76b2b57a9f75282c8ffb0538ce7073021f16e0
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-04-23 17:08:03 +0200

    Removed install build and log directories, updated .gitignore

--- install/turtlebot3_gazebo/share/turtlebot3_gazebo/cmake/ament_cmake_export_dependencies-extras.cmake
+++
@@ -1,1 +0,0 @@
-/home/piotr/turtlebot3_ws/build/turtlebot3_gazebo/ament_cmake_export_dependencies/ament_cmake_export_dependencies-extras.cmake
--- src/build/py_pubsub/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/piotr/turtlebot3_ws/src/install/py_pubsub'
